5bc8dd0f6aedc8d9c7b033aa35337ee1eb479cb2
Gabriel Vieira <gabriellpmw@gmail.com>
2025-03-14 21:54:40 -0300

    Implementação do radix sort

---
+++ src/Algoritmo_de_ordenacao/RadixSort.java
@@ -0,0 +1,92 @@
+package Algoritmo_de_ordenacao;
+
+import java.util.Arrays;
+
+public class RadixSort {
+
+    public static void contador_de_frequencia(int[] array, int divisor) {
+        int tamanho = array.length;
+        int[] contagem = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int[] arrayOrdenado = new int[tamanho];
+
+        for (int numero : array) {
+            int digito = (numero / divisor) % 10;
+            contagem[digito]++;
+        }
+        /*
+        * {123, 321, 543, 1, 89, 1, 324, 77, 123}
+        * [0, 3, 0, 3, 1, 0, 0, 1, 0, 1] - Unidade
+        * [2, 0, 4, 0, 1, 0, 0, 1, 1, 0] - Dezena
+        * [4, 2, 0, 2, 0, 1, 0, 0, 0, 0] - Centena
+        * */
+
+        int acumulador = 0;
+        for (int i = 0; i < 10; i++) {
+            int temp = contagem[i];
+            contagem[i] = acumulador;
+            acumulador += temp;
+        }
+        /*
+        * {123, 321, 543, 1, 89, 1, 324, 77, 123}
+        * [0, 3, 0, 3, 1, 0, 0, 1, 0, 1] - Unidade
+        * Iteração 1 (i = 0):
+        * Frequência: contagem[0] = 0.
+        * Posição inicial: contagem[0] = 0.
+        * Atualiza acumulador: 0 + 0 = 0.
+        * Agora, o dígito 0 começa na posição 0, e o próximo dígito (1) começará na posição 0.
+        *
+        * Iteração 2 (i = 1):
+        * Frequência: contagem[1] = 3.
+        * Posição inicial: contagem[1] = 0.
+        * Atualiza acumulador: 0 + 3 = 3.
+        * Agora, o dígito 1 começa na posição 0, e o próximo dígito (2) começará na posição 3.
+        *
+        * Iteração 3 (i = 2):
+        * Frequência: contagem[2] = 0.
+        * Posição inicial: contagem[2] = 3.
+        * Atualiza acumulador: 3 + 0 = 3.
+        * Agora, o dígito 2 começa na posição 3, e o próximo dígito (3) começará na posição 3.
+        *
+        * [0, 0, 3, 3, 6, 7, 7, 7, 8, 8] - Unidade
+        * [0, 2, 2, 6, 6, 7, 7, 7, 8, 9] - Dezena
+        * [0, 4, 6, 6, 8, 8, 9, 9, 9, 9] - Centena
+        */
+
+        for (int numero : array) {
+            int digito = (numero / divisor) % 10;
+            arrayOrdenado[contagem[digito]] = numero;
+            contagem[digito]++;
+        }
+
+        for (int i = 0; i < tamanho; i++) {
+            array[i] = arrayOrdenado[i];
+        }
+    }
+
+    public static void radixSort(int[] array) {
+        int maiorValor = Arrays.stream(array).max().getAsInt();
+
+        for (int divisor = 1; maiorValor / divisor > 0; divisor *= 10) {
+            contador_de_frequencia(array, divisor);
+        }
+    }
+
+    public static void exibirArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Array exemplo:         int[] numeros = {123, 321, 543, 1, 89, 1, 324, 77, 123};
+        int[] numeros = {123, 321, 543, 1, 89, 1, 324, 77, 123};
+        System.out.println("Antes da ordenação: ");
+        exibirArray(numeros);
+
+        radixSort(numeros);
+
+        System.out.println("Depois da ordenação: ");
+        exibirArray(numeros);
+    }
+}
